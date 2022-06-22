2ebd0d31bf606e915c648a4e9de96aa5bcfe0a6a
Subhodeep000 <97718155+Subhodeep000@users.noreply.github.com>
2022-06-22 19:59:52 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,123 @@
+let allMusic = [
+  {
+    name: "REMIX",
+    artist: "Astif Aslam",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "CHUNAR",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "DECEMBER ER SOHORE",
+    artist: "Chilakotha",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "KHAMOSHIYAN",
+    artist: "Arijit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "KYA MUJHE PAYER HE",
+    artist: "KK",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "O Re PRIYA",
+    artist: "Rahat Fateh Ali Khan",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "EKLA GHOR",
+    artist: "Rupam Islam(Fossiles)",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "JIBON",
+    artist: "Rupam Islam(Fossiles)",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "ALL TIME HITS OF KK",
+    artist: "KK",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "AMADER SURJO MERUN",
+    artist: "Arijit Singh",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "ARO EKBER",
+    artist: "Rupam Islam(Fossiles)",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "BEGHI BEGHI",
+    artist: "James",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "HASTE DEKHO",
+    artist: "A.Bakchu",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "MEGH BOLECHA",
+    artist: "unknown",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "KAISA HUA",
+    artist: "",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "O JE MANE NA MANA",
+    artist: "unknown",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "PRITHIBI TA NAKI CHOTO HOTE HOTE",
+    artist: "",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "HAMARI ADHURI KAHANI",
+    artist: "Arijit SIngh",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "SEI TUMI",
+    artist: "Nobelman",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "TUMI JAO",
+    artist: "",
+    img: "music-20",
+    src: "music-20"
+  },
+  
+];
